12c212eb09c1ad0366d9e34d77cf86489cdab826
BingChilllling <119667055+BingChilllling@users.noreply.github.com>
2023-04-08 15:21:29 -0400

    changed backend location

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -40,7 +40,7 @@ const Item = ({item, width}) => {
                     alt={item.name}
                     width="300px"
                     height="400px"
-                    src={`http://localhost:1337${url}`}
+                    src={`https://strapi-yszw.onrender.com${url}`}
                     onClick={() => navigate(`/item/${item.id}`)}
                     style={{ cursor: "pointer" }}
                 />
